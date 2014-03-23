aaaf2687da660581be8b3122567892f294c2770c
Scavy <svg@scavy.net>
2014-03-23 01:25:07 +0100

    Updated the test - Once again

--- src/net/scavy/sfactor/Sfactor.java
+++ src/net/scavy/sfactor/Sfactor.java
@@ -8,6 +8,8 @@ package net.scavy.sfactor;
  */
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.*;
 
 public class Sfactor {
@@ -15,16 +17,27 @@ public class Sfactor {
         createWindow();
     }
     private static void createWindow() {
-            //Create and set up the window.
-            JFrame frame = new JFrame("Scavy Simple GUI");
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //Create and set up the window.
+        JFrame frame = new JFrame("Scavy Simple GUI");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(400, 400));
 
-            JLabel textLabel = new JLabel("Test label",SwingConstants.CENTER);
-            textLabel.setPreferredSize(new Dimension(300, 100));
-            frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+        JLabel textLabel = new JLabel("Test label", SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
 
-            frame.setLocationRelativeTo(null);
-            frame.pack();
-            frame.setVisible(true);
+        JButton closeButton = new JButton("Close");
+        closeButton.setPreferredSize(new Dimension(100, 20));
+        frame.getContentPane().add(closeButton, BorderLayout.SOUTH);
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
